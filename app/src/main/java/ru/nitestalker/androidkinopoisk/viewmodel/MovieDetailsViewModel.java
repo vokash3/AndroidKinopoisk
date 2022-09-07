141e95f46ec6bec7e11ebeb6ba03d3b8ca43c6a5
wain_m1 <vokash3@yandex.ru>
2022-09-07 13:43:33 +0300

    * оператор .map(new Function<Приходящий_тип, новый_тип )>, который позволяет преобразовать старый приходящий тип в новый (показан в MovieDetailsViewModel.loadTrailers). * Далее, уже новый тип передаётся на обработку оператору .subscribe в метод accept().

--- app/src/main/java/ru/nitestalker/androidkinopoisk/viewmodel/MovieDetailsViewModel.java
+++ app/src/main/java/ru/nitestalker/androidkinopoisk/viewmodel/MovieDetailsViewModel.java
@@ -13,6 +13,7 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.Getter;
 import ru.nitestalker.androidkinopoisk.model.TrailersResponse;
@@ -36,11 +37,18 @@ public class MovieDetailsViewModel extends AndroidViewModel {
         Disposable disposable = ApiFactory.apiService.loadTrailers(id)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<TrailersResponse>() {
+                .map(new Function<TrailersResponse, List<Trailer>>() { // Преобразуем TrailerResponse в List<Trailer>. Грубо говоря, распаковываем респонс и передаём далее по цепочке список из респонса
+
                     @Override
-                    public void accept(TrailersResponse trailersResponse) throws Throwable {
-                        listTrailers.setValue(trailersResponse.getTrailersList().getTrailers());
-                        Log.d(TAG, trailersResponse.toString());
+                    public List<Trailer> apply(TrailersResponse trailersResponse) throws Throwable {
+                        return trailersResponse.getTrailersList().getTrailers();
+                    }
+                })
+                .subscribe(new Consumer<List<Trailer>>() {
+                    @Override
+                    public void accept(List<Trailer> trailers) throws Throwable {
+                        listTrailers.setValue(trailers);
+                        Log.d(TAG, trailers.toString());
                     }
                 }, throwable -> {
                     Log.e(TAG, throwable.toString());
